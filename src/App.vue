aceb009ff52dec9287b412812e4c3405999710ba
Isac Pessoa <isacdavidpessoa@hotmail.com>
2023-09-18 18:31:54 -0300

    commitado

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,64 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { reactive } from 'vue';
+
+  const estado = reactive({
+    filtro: 'soma',
+    numero1: 0,
+    numero2: 0,
+    resultado: 0
+  })
+
+  const operacao = ()=> {
+    const { filtro, numero1, numero2 } = estado;
+
+    switch(filtro){
+      case 'soma':
+        return numero1 + numero2;
+      case 'subtracao':
+        return numero1 - numero2;
+      case 'multiplicacao':
+        return numero1 * numero2;
+      case 'divisao':
+        return numero1 / numero2;
+    }
+
+  }
+
+  function calcularResultado(evento) {
+  estado.resultado = operacao();
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Calculadora Aritimética</h1>
+    </header>
+    
+    <form>
+      <div class="row">
+        <div class="col-md-3">
+          <input v-model="estado.numero1" @input="calcularResultado" class="form-control mb-3" type="number" placeholder="Primeiro Número">
+          <input v-model="estado.numero2" @input="calcularResultado" class="form-control" type="number" placeholder="Segundo Número">
+        </div>
+        <div class="col-md-2">
+          <select v-model="estado.filtro" @change="calcularResultado" class="form-control">
+            <option value="soma">soma</option>
+            <option value="subtracao">subtração</option>
+            <option value="multiplicacao">multiplicação</option>
+            <option value="divisao">divisão</option>
+          </select>
+        </div>
+      </div>
+    </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="mt-3">
+      <p>Resultado: {{ estado.resultado }}</p>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
